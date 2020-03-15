2d96a219409b64b4642ee79cda8da668533cbf9b
ngoclog <62193504+ngoclog@users.noreply.github.com>
2020-03-15 09:35:46 +0700

    Update and rename stream_1.js to stream_2.js

--- stream_2.js
+++ stream_2.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var creampie = "https://www.solidfiles.com/e/";
 	$('a[href^="https://www.solidfiles.com/v/"]').each(function(){ 
 		var Janda = $(this).attr("href"); 
-		var Perawan = Janda.replace("http://www.solidfiles.com/v/", creampie); 
+		var Perawan = Janda.replace("https://www.solidfiles.com/v/", creampie); 
 		$(this).attr("id", "crot"); 
 		$(this).attr("href", Perawan); 
 		var ubah = document.getElementById("crot").href;
